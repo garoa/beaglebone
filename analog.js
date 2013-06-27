19fd5df08bdb4700fa8a935921a26ff81782a4fc
Luciano Ramalho <luciano@ramalho.org>
2013-06-27 00:25:08 -0300

    animacao funcionando, totalmente assincrona

--- analog.js
+++ analog.js
@@ -1,12 +1,14 @@
 var b = require('bonescript');
-function ler() {
+var exibir = function exibir(p) {
+  console.log(p.value);
+};
+var ler = function ler() {
   b.analogRead('P9_39', exibir);
-}
-var i = 100;
-function exibir(pin) {
-    if (pin) console.log(pin.value);
-    i--;
-    if (i > 0) setTimeout(ler, 100);
-}
-ler();
+};
+var timer = setInterval(ler, 50);
+var parar = function parar() {
+  clearInterval(timer);
+};
+
+setTimeout(parar, 10000);
 
